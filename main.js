62cd3e01f7ef21696b198b405cdbb583047fb1ab
saphiraRat <luis.devmot@gmail.com>
2022-11-05 14:56:29 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function createGame(player1, hour, player2) {
+  return `
+     <li>
+     <img src="./Assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+     <strong>${hour}</strong>
+     <img src="Assets/icon-${player2}.svg" alt="Bandeira de ${player2}" />
+      </li>
+  `
+}
+
+let delay = -0.5;
+function createCard(date, day, games) {
+  delay = delay + 0.5
+  return `
+          <div class="card" style="animation-delay: ${delay}s">
+          <h2>${date} <span>${day}</span></h2>
+          <ul>
+           ${games} 
+          </ul>
+        </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =   
+        createCard(
+          "24/11","quinta",
+          createGame("brazil", "16:00", "serbia")) +
+
+        createCard(
+          "28/11","segunda",
+          createGame("switzerland", "13:00", "brazil")) +
+
+        createCard(
+          "02/12","sexta",
+          createGame("brazil", "16:00", "cameroon") )
+        
